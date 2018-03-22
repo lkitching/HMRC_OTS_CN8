1d4498a12be7093d35892332076f7c49ea2ff458
Alex Tucker <alex@floop.org.uk>
2018-03-22 16:27:47 +0000

    Update table2qb library and build script.

--- build.clj
+++ build.clj
@@ -1,6 +1,5 @@
 (require 'table2qb.core)
 (ns table2qb.core)
 (components-pipeline "metadata/components.csv" "out")
-(codelist-pipeline "metadata/flow-directions.csv" "out" "Flow directions" "flow-directions")
-(codelist-pipeline "metadata/units.csv" "out" "Measurement units" "measurement-units")
-(data-pipeline "in/CN8_Non-EU_cod_2012.csv" "out" "HMRC CN8" "hmrc-cn8")
+(codelist-pipeline "pre/countries.csv" "out" "Countries" "countries")
+(data-pipeline "pre/CN8_Non-EU_cod_2012.csv" "out" "HMRC Overseas Trade by CN8" "hmrc-ots-cn8")
